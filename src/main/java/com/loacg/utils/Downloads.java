9ccde42f04aaccd8c27f5dcd2688f1837e78df72
Sendya <18x@loacg.com>
2017-02-14 10:14:41 +0800

    Fixed import error remove mavenCentral

--- src/main/java/com/loacg/utils/Downloads.java
+++ src/main/java/com/loacg/utils/Downloads.java
@@ -1,7 +1,6 @@
 package com.loacg.utils;
 
 import com.loacg.entity.FileDown;
-import javassist.bytecode.analysis.Executor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;

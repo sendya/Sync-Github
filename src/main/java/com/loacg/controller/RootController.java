9475eded9932c60ed3718324fa40fab488553dff
Sendya <18x@loacg.com>
2017-05-12 04:21:25 +0800

    Add Manual execution

--- src/main/java/com/loacg/controller/RootController.java
+++ src/main/java/com/loacg/controller/RootController.java
@@ -1,6 +1,7 @@
 package com.loacg.controller;
 
 import com.loacg.bootstrap.AppEnv;
+import com.loacg.github.SyncDaemon;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,8 +18,17 @@ public class RootController {
     @Autowired
     private AppEnv appEnv;
 
+    @Autowired
+    private SyncDaemon daemon;
+
     @RequestMapping("/")
     public String home() {
         return String.format("save path: %s, thread: %s, max thread: %s", appEnv.getSavePath(), appEnv.getSingleFileDownloadThread(), appEnv.getMaxThread());
+    }
+
+    @RequestMapping("/start")
+    public String start() {
+        daemon.sync();
+        return "success";
     }
 }
